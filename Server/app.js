60560d518ca6c7f6d5fdfb3813f87e62989e9f90
cwwei <caiweiwei@whu.edu.cn>
2023-04-19 10:22:58 +0800

    地图下钻

--- Server/app.js
+++ Server/app.js
@@ -2,17 +2,19 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/main.html'));
-})
+app.use('/public', express.static('static'));
 
-app.get('/main.js' , function (req, res) {
-    res.sendFile(path.join(__dirname, 'js/main.js'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+/*app.get('/main.js' , function (req, res) {
+    res.sendFile(path.join(__dirname, 'static/js/main.js'));
 })
 
 app.get('/data.json', function (req, res) {
     res.sendFile(path.join(__dirname, '/json/data.json'));
-})
+})*/
 
 app.listen(80, function () {
     console.log('Node server is running..');
